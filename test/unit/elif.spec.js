4a1fd904793fd99d0098e988b81919284936e2df
Zach Snow <z@zachsnow.com>
2015-11-08 11:37:12 -0800

    Tests that do nothing and have bugs, eesh.

--- test/unit/elif.spec.js
+++ test/unit/elif.spec.js
@@ -8,10 +8,6 @@
     $doc = angular.element(doc.body);
     $doc[0].documentElement = $doc[0];
 
-    module('toggles', function($provide){
-      $provide.value('$document', $doc);
-    });
-
     inject(function($animate){
       // Override animate so it happens immediately for easier testing.
       $animate.enter = function(clone, __, element){
