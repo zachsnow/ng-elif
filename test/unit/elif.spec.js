20af06c05c2c99581da6e79acbbf57f5fb9903b0
Zach Snow <z@zachsnow.com>
2015-11-09 09:38:18 -0800

    Remove unused function -- copy-paste is bad mmkay.

--- test/unit/elif.spec.js
+++ test/unit/elif.spec.js
@@ -1,26 +1,6 @@
 (function () {
   'use strict';
 
-  var $doc;
-
-  var setup = function(){
-    var doc = document.implementation.createHTMLDocument();
-    $doc = angular.element(doc.body);
-    $doc[0].documentElement = $doc[0];
-
-    inject(function($animate){
-      // Override animate so it happens immediately for easier testing.
-      $animate.enter = function(clone, __, element){
-        element.append(clone);
-      };
-    });
-  };
-
-})();
-
-(function () {
-  'use strict';
-
   var text = function(e){
     return angular.element(e).text();
   };
